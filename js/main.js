b256675a9aa27785f714fa8fe5c26a34a888a8ab
Eyal Benmoshe <eyalbenmoshe@Efrains-iMac.local>
2018-08-20 16:58:34 -0700

    finished ipad for header and first section

--- js/main.js
+++ js/main.js
@@ -169,6 +169,16 @@ var slider = function() {
 
 	slider();
 
+/// hamburger rotate code
+	$('.mobile-nav-button').on('click', function() {
+		$( ".mobile-nav-button .mobile-nav-button__line:nth-of-type(1)" ).toggleClass( "mobile-nav-button__line--1");
+		$( ".mobile-nav-button .mobile-nav-button__line:nth-of-type(2)" ).toggleClass( "mobile-nav-button__line--2");  
+		$( ".mobile-nav-button .mobile-nav-button__line:nth-of-type(3)" ).toggleClass( "mobile-nav-button__line--3");  
+
+		$('.mobile-menu').toggleClass('mobile-menu--open');
+		return false;
+	}); 
+
 }); // end of document function
 
 // adding box shadow when navbar scrolls down
